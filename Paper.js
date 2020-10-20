5f710f1b09339fc8d40807bfeba033252e79c0da
YuvrajMunjal <40139552+YuvrajMunjal@users.noreply.github.com>
2020-10-20 13:03:00 +0530

    Add files via upload

--- Paper.js
+++ Paper.js
@@ -1,6 +1,7 @@
 class Paper {
-
+    
     constructor(x,y,radius,options){
+        this.image = loadImage("Images/paper.png");
         var options = {         
             isStatic: false,
             restitution: 0.8,
@@ -12,7 +13,8 @@ class Paper {
         }   
         display(){
             var pos =this.body.position;
-           
+            imageMode(CENTER);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
             ellipseMode(RADIUS);
             ellipse(pos.x, pos.y, this.radius);
             
